15d9b523482ed578aedd8665cfde2f1e2cec7bcb
ArkNito <b-teme@mail.ru>
2024-04-13 18:32:29 +0300

    Выравнивание, удаление мусора

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class Alarm : MonoBehaviour
 {
-    private bool _isAlarmOn;
-    private AudioSource _audio;
+	private bool _isAlarmOn;
+	private AudioSource _audio;
 	private float _volumeChange = 0.0003f;
 	private float _volumeMultiplier = 2;
 	private float _alarmVolume = 0.002f;
 
 	public void Start()
-    {
+	{
 		_audio = GetComponent<AudioSource>();
 		_isAlarmOn = false;
 	}
 
 	public void Update()
-    {
-        if (_isAlarmOn)
-            _audio.volume *= _volumeMultiplier;
-        else
+	{
+		if (_isAlarmOn)
+			_audio.volume *= _volumeMultiplier;
+		else
 			_audio.volume -= _volumeChange;
 	}
 
